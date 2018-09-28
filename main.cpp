5034c594b64caffaa7c81a4ba914115ce147d2f9
kbuffardi <kbuffardi@ecc-jaguar.csuchico.edu>
2018-09-28 10:50:35 -0700

    Loops through each character and changes their ASCII values

--- main.cpp
+++ main.cpp
@@ -11,6 +11,11 @@ int main()
   cout<<"What do you say?\n";
   getline(cin,said);
 
+  for(int index=0; index<said.length(); index++)
+  {
+    char converted = said[index] - ('a' - 'A');
+    cout<<said[index]<<" "<<converted<<endl;
+  }
   cout<<"As a shout, you said:\n"<<said<<endl;
 
   return 0;
